239acfbd68da4caab12f2177fd369417d5e6f184
Emma Gerigscott <elgerig@gmail.com>
2023-08-03 09:20:25 -0700

    add project info file

---
+++ src/assets/content/projectData.ts
@@ -0,0 +1,26 @@
+// psuedo-db for all project info
+const allProjects = [
+  {
+    id: 1,
+    title: "Buckey's One Stop Art Shop",
+    description: "My capstone project for Epicodus: An e-commerce website that allows an artist (admin) to upload artworks to a database and select a timeframe to sell a single piece of art. The user sees the art, the count-down timer, and a buy button. The user can add the art to their cart! This is still in developement and uses Typescript, React, Firebase, and Vite.",
+    imgSrc: "./src/assets/img/",
+    link: "url to webpage"
+  },
+  {
+    id: 2,
+    title: "Corn Shack",
+    description: "Get your corn at the Corn Shack! A React + JS website displaying types of corn that the user can click on to then buy from a bushel. The user even gets a corn wallet! Click on corn facts to read some corn facts. Add your own corn variety to the Corn Shack, if you like.",
+    imgSrc: "./src/assets/img/",
+    link: "url to webpage"
+  },
+  {
+    id: 3,
+    title: "Factory.Solution",
+    description: "Manage machines and engineers for your international factory. Built with C#, ASP.NET Core, MVC, MySQL, and Entity Framework Core.",
+    imgSrc: "./src/assets/img/",
+    link: "url to webpage"
+  }
+]
+
+export default allProjects;
