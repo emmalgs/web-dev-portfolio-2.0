269cef43cf6628040f5ecd1a6cfd4300fb1f59fb
Emma Gerigscott <elgerig@gmail.com>
2023-08-03 16:27:05 -0700

    basic header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <div className="header">
+      <div className="header-line"></div>
+      <div className="header-circle-big"></div>
+      <div className="header-circle-small"></div>
+    </div>
+  );
+}
+
+export default Header;
