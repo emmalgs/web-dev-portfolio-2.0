fc518e46dc0d66189ed584a8d2a1809c879d49e8
Emma Gerigscott <elgerig@gmail.com>
2023-08-07 10:53:40 -0700

    add details view

--- src/components/ProjectDetails.tsx
+++ src/components/ProjectDetails.tsx
@@ -2,10 +2,23 @@ import { ProjectDetailsProps } from "../types";
 
 function ProjectDetails(props: ProjectDetailsProps) {
   return (
-    <>
-      <h1>Project Details</h1>
-    </>
-  )
+    <div className="project-details-main">
+      <div className="pd-back-btn" onClick={props.back}></div>
+      <h1 className="pd-title">{props.project.title}</h1>
+      <div
+        className="project-image-detail"
+        style={{
+          backgroundImage: `url(${props.project.imgSrc})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      ></div>
+      <p className="pd-desc">{props.project.description}</p>
+      <p className="pd-link">
+        <a href={props.project.link}>Link to Project</a>
+      </p>
+    </div>
+  );
 }
 
 export default ProjectDetails;
