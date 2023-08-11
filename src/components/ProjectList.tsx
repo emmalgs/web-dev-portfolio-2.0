2432ef8d0f9627769be69177ffc1450cd066d735
Emma Gerigscott <elgerig@gmail.com>
2023-08-11 08:51:07 -0700

    add react style to project list

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -1,10 +1,15 @@
 import { ProjectListProps } from "../types";
 
 function ProjectList(props: ProjectListProps) {
-
   return (
     <div className="projects-main">
-      <h1>Projects</h1>
+      <h1
+        style={{
+          fontStyle: "italic",
+        }}
+      >
+        PROJECTS
+      </h1>
       {props.list.map((project) => {
         return (
           <div className="project-card" key={project.id}>
@@ -15,14 +20,17 @@ function ProjectList(props: ProjectListProps) {
             <h3 className="pc-title">{project.title}</h3>
             <p className="pc-desc">{project.description}</p>
             <p className="pc-link">
-              <a href={project.link}>View Code</a>
+              <a href={project.link} target="blank">View Code</a>
             </p>
-            <div className="pc-img" style={{
-              backgroundImage: `url(${project.imgSrc})`,
-              backgroundSize: 'cover',
-              backgroundPosition: 'center'
-            }}>
-            </div>
+            <div
+              className="pc-img"
+              style={{
+                backgroundImage: `url(${project.imgSrc})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                opacity: "50%",
+              }}
+            ></div>
           </div>
         );
       })}
