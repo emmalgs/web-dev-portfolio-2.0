149d632650ad78510aa70ee9f9663cdc46d1ba78
Emma Gerigscott <elgerig@gmail.com>
2023-10-24 11:57:31 -0700

    import navbar to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,15 @@
+import NavBar from "./molecules/navbar";
+
 function Header() {
   return (
     <div className="header">
+      <NavBar
+        links={[
+          { name: "Projects", path: "/projects" },
+          { name: "About", path: "/about" },
+          { name: "Contact", path: "/contact" },
+        ]}
+      />
       <div className="header-line"></div>
       <div className="header-circle-big"></div>
       <div className="header-circle-small"></div>
