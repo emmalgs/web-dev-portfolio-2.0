71e418e5585ce5ea9e829329d4839fdce285b02d
Emma Gerigscott <elgerig@gmail.com>
2023-10-24 13:11:28 -0700

    add display dropdown nav logic

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,21 @@
 import NavBar from "./molecules/navbar";
+import { useState } from "react";
 
 function Header() {
-  return (
-    <div className="header">
+  const [displayNav, setDisplayNav] = useState(false);
+
+  const displayDropDownNav = () => {
+    setDisplayNav(true);
+    console.log(displayNav);
+  };
+
+  const hideDropDownNav = () => {
+    setDisplayNav(false);
+    console.log(displayNav);
+  }
+
+  const dropDownNav = () => {
+    return (
       <NavBar
         links={[
           { name: "Projects", path: "/projects" },
@@ -10,9 +23,18 @@ function Header() {
           { name: "Contact", path: "/contact" },
         ]}
       />
+    );
+  };
+
+  return (
+    <div className="header" onMouseEnter={displayDropDownNav} onMouseLeave={hideDropDownNav}>
       <div className="header-line"></div>
       <div className="header-circle-big"></div>
-      <div className="header-circle-small"></div>
+      <div
+        className="header-circle-small"
+
+      ></div>
+      <div>{displayNav ? dropDownNav() : null}</div>
     </div>
   );
 }
