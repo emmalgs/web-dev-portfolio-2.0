b7eb79bb86e2f3749b7a43fa9476bc7b52ff263e
Emma Gerigscott <elgerig@gmail.com>
2023-10-28 17:01:45 -0700

    add classname to nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,29 +10,32 @@ function Header() {
 
   const hideDropDownNav = () => {
     setDisplayNav(false);
-  }
+  };
 
   const dropDownNav = () => {
     return (
-      <NavBar
-        links={[
-          { name: "Projects", path: "/projects" },
-          { name: "About", path: "/about" },
-          { name: "Contact", path: "/contact" },
-          { name: "Home", path: "/"}
-        ]}
-      />
+      <div className="header-nav">
+        <NavBar
+          links={[
+            { name: "Projects", path: "/projects" },
+            { name: "About", path: "/about" },
+            { name: "Contact", path: "/contact" },
+            { name: "Home", path: "/" },
+          ]}
+        />
+      </div>
     );
   };
 
   return (
-    <div className="header" onMouseEnter={displayDropDownNav} onMouseLeave={hideDropDownNav}>
+    <div
+      className="header"
+      onMouseEnter={displayDropDownNav}
+      onMouseLeave={hideDropDownNav}
+    >
       <div className="header-line"></div>
       <div className="header-circle-big"></div>
-      <div
-        className="header-circle-small"
-
-      ></div>
+      <div className="header-circle-small"></div>
       <div>{displayNav ? dropDownNav() : null}</div>
     </div>
   );
